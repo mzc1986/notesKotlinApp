cf2787514cb338a7bba69212e6d64a8feaccc121
adolescent <mashrufz@gmail.com>
2018-12-09 22:36:58 +0100

    first commit

---
+++ app/src/main/java/com/ds/notekeeper/Contants.kt
@@ -0,0 +1,4 @@
+package com.ds.notekeeper
+
+const val NOTE_POSITION = "NOTE_POSITION"
+const val NOTES_NOT_SET = -1
---
+++ app/src/main/java/com/ds/notekeeper/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.ds.notekeeper
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var notesPosition = NOTES_NOT_SET
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item,
+            DataManager.courses.values.toList())
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
+
+        notesPosition = savedInstanceState?.getInt(NOTE_POSITION, NOTES_NOT_SET) ?:
+                intent.getIntExtra(NOTE_POSITION, NOTES_NOT_SET)
+
+        if(notesPosition != NOTES_NOT_SET)
+            displayNote()
+        else {
+            DataManager.notes.add(NoteInfo())
+            notesPosition = DataManager.notes.lastIndex
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(NOTE_POSITION, notesPosition)
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notesPosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun moveNext() {
+        ++notesPosition
+        displayNote()
+        invalidateOptionsMenu()
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+
+        if(notesPosition >= DataManager.notes.lastIndex){
+            val menuItem = menu?.findItem(R.id.action_next)
+            if(menuItem != null){
+                menuItem.icon = getDrawable(R.drawable.ic_block_white_24dp)
+                menuItem.isEnabled = false
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        saveNote()
+    }
+
+    private fun saveNote() {
+
+        val note = DataManager.notes[notesPosition]
+        note.title = textNoteTitle.text.toString()
+        note.text = textNoteText.text.toString()
+        note.course = spinnerCourses.selectedItem as CourseInfo
+    }
+}
